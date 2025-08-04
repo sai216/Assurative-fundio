17a4fb3e197a186a8d1f70e29437ac7d73f3dca5
Saikiran <sai521024@gmail.com>
2025-08-04 06:08:32 +0000

    fix: removed stream-browserify and unnecessary fallbacks

--- next.config.js
+++ next.config.js
@@ -3,41 +3,17 @@ const nextConfig = {
   webpack: (config, { isServer, webpack }) => {
     if (!isServer) {
       config.resolve.fallback = {
-        ...config.resolve.fallback,
         fs: false,
         net: false,
         tls: false,
-        // Removed crypto-browserify
-        stream: require.resolve('stream-browserify'),
-        buffer: require.resolve('buffer'),
-        util: require.resolve('util'),
-        url: require.resolve('url'),
-        assert: require.resolve('assert'),
       };
-
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser',
-        })
-      );
     }
 
-    // Remove duplicate alias declaration
     config.resolve.alias = {
       ...config.resolve.alias,
       '@injectivelabs/token-metadata': false,
       '@injectivelabs/sdk-ts': false,
     };
-
-    config.externals = [...(config.externals || [])];
-
-    if (!isServer) {
-      config.externals.push({
-        '@injectivelabs/sdk-ts': 'commonjs @injectivelabs/sdk-ts',
-        '@injectivelabs/token-metadata': 'commonjs @injectivelabs/token-metadata',
-      });
-    }
 
     config.experiments = {
       ...config.experiments,
